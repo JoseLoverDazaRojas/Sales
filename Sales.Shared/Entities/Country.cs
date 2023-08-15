24b1e82cfef82d35b31db61f237125ed99966af2
Jose Lover Daza Rojas <josedaza1121437@correo.itm.edu.co>
2023-08-14 22:48:31 -0500

    Add Project

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,28 @@
+﻿namespace Sales.Shared.Entities
+{
+    #region Import
+
+    using System.ComponentModel.DataAnnotations;
+
+    #endregion Import
+
+    /// <summary>
+    /// The class Country
+    /// </summary>
+
+    public class Country
+    {
+
+        #region Attributes
+
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+
+        #endregion Attributes
+
+    }
+}
