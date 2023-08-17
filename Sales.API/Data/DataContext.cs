8895f23b147fa68413225c4ceb16ee4ac7772bce
Jose Lover Daza Rojas <josedaza1121437@correo.itm.edu.co>
2023-08-16 19:57:45 -0500

    add cnection

---
+++ Sales.API/Data/DataContext.cs
@@ -0,0 +1,42 @@
+﻿namespace Sales.API.Data
+{
+    #region Import
+
+    using Microsoft.EntityFrameworkCore;
+    using Sales.Shared.Entities;
+
+    #endregion Import
+
+    /// <summary>
+    /// The class DataContext
+    /// </summary>
+
+    public class DataContext : DbContext
+    {
+
+        #region Constructor
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        #endregion Constructor
+
+        #region Entities
+
+        public DbSet<Country> Countries { get; set; }
+
+        #endregion Entities
+
+        #region Model
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+
+        #endregion Model
+
+    }
+}
